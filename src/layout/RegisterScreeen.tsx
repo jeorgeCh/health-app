fc986aa2c98c7ad3cd4f142cfa6437d52d8fdf74
Jorge <jorgechicaiza2512@gmail.com>
2025-04-08 21:57:46 -0500

    Register log

--- src/layout/RegisterScreeen.tsx
+++ src/layout/RegisterScreeen.tsx
@@ -1,23 +1,62 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native"
+import { supabase } from "../supabaseClient"
 
 export default function RegisterScreeen({ onClose }: any) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [fullname, setFullname] = useState("");
+    const [mobilephone, setMobilephone] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrormessage] = useState("");
+
+    const handleRegister = async () => {
+        setLoading(true);
+        setErrormessage("");
+         
+        const{data, error}= await supabase.auth.signUp({
+            email,password
+        });
+        if(error){
+            setErrormessage(error.message);
+            setLoading(false);
+            return;
+        }
+    //Insert data into SUPABASE 
+    const { error: InsertError } = await supabase.from("users").insert([
+        {
+            email: email,
+            password: password,
+            fullname: fullname,
+            mobile_phone: mobilephone
+        }
+    ])
+    if (InsertError){
+        setErrormessage(InsertError.message);
+        setLoading(false);
+        
+    }else{
+        alert("User has been created")
+        onClose();
+    }
+    }
+    
     return (
         <View style={styles.container}>
             <Text style={styles.title}> Sing up</Text>
-    
+
             <TextInput
                 style={styles.input}
                 placeholder="Usuario@gmail.com"></TextInput>
-           
+
             <TextInput style={styles.input}
                 placeholder="***************" secureTextEntry></TextInput>
-           
+
 
             <TextInput style={styles.input}
                 placeholder="Your fullname" secureTextEntry></TextInput>
 
-            
+
             <TextInput style={styles.input}
                 placeholder="(+57) 000000" secureTextEntry></TextInput>
 
